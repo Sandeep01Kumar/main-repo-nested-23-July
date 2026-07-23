26ecc3d4e8c314a4b0e9a47813da18350f8ae5ce
Sandeep01Kumar <sandeep@blitzy.com>
2026-07-23 15:02:30 +0530

    Update index.js

--- app/index.js
+++ app/index.js
@@ -1,3 +1,15 @@
+const http = require("http");
 
-console.log("Main Repository");
-console.log("Testing Git submodules...");
+const server = http.createServer((req, res) => {
+  if (req.url === "/") {
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.end("Hello World");
+  } else {
+    res.writeHead(404);
+    res.end("Not Found");
+  }
+});
+
+server.listen(3000, () => {
+  console.log("Server running on http://localhost:3000");
+});
